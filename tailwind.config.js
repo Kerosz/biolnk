3e251662da82edcddfe770dfdba57171696697c2
Kerosz <andrei_chirila@outlook.com>
2021-12-02 15:35:50 +0200

    feat: deps and tooling setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+    typography: ["dark"],
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
