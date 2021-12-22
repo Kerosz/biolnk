5ea1c4faae3b21a4af7aa03f42d8a7b24f57ec44
Kerosz <andrei_chirila@outlook.com>
2021-12-22 13:11:39 +0200

    chore(studio): extract page navigation links

--- apps/studio/src/components/PageNavigation.tsx
+++ apps/studio/src/components/PageNavigation.tsx
@@ -2,29 +2,35 @@ import { Text, Flex } from "@biolnk/ui";
 import { Routes } from "~/data/enums/routes";
 import Link from "./common/Link";
 
+const NAV_LINKS = [
+  {
+    path: Routes.DASHBOARD,
+    label: "Overview",
+  },
+  {
+    path: Routes.DESIGN,
+    label: "Design",
+  },
+  {
+    path: Routes.ANALYTICS,
+    label: "Analytics",
+  },
+  {
+    path: Routes.ACCOUNT,
+    label: "Account",
+  },
+];
+
 const PageNavigation: React.FC = () => {
   return (
     <Flex as="ul" className="space-x-8 mb-8 border-b-2 border-mauve-600">
-      <li className="py-4">
-        <Link url={Routes.DASHBOARD}>
-          <Text>Overview</Text>
-        </Link>
-      </li>
-      <li className="py-4">
-        <Link url={Routes.DESIGN}>
-          <Text>Design</Text>
-        </Link>
-      </li>
-      <li className="py-4">
-        <Link url={Routes.ANALYTICS}>
-          <Text>Analytics</Text>
-        </Link>
-      </li>
-      <li className="py-4">
-        <Link url={Routes.ACCOUNT}>
-          <Text>Account</Text>
-        </Link>
-      </li>
+      {NAV_LINKS.map(({ path, label }, idx) => (
+        <li key={`pn-links_${idx}`} className="py-4">
+          <Link url={path}>
+            <Text>{label}</Text>
+          </Link>
+        </li>
+      ))}
     </Flex>
   );
 };
