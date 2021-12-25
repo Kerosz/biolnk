f17376a09f06ec6db8fac4049bfce93e8dee12fb
Kerosz <andrei_chirila@outlook.com>
2021-12-25 14:53:21 +0200

    chore(studio): add db related enums && route additions

---
+++ apps/studio/src/data/enums/db.ts
@@ -0,0 +1,15 @@
+export enum UserStatus {
+  BASIC = "basic",
+  VERIFIED = "verified",
+}
+
+export enum PageLinkPreference {
+  PATH = "path",
+  SUBDOMAIN = "subdomain",
+  CUSTOM = "custom",
+}
+
+export enum LinkKind {
+  DEFAULT = "default",
+  ICON = "icon",
+}
--- apps/studio/src/data/enums/routes.ts
+++ apps/studio/src/data/enums/routes.ts
@@ -1,4 +1,5 @@
 export enum Routes {
+  ENTRY = "/",
   SIGNIN = "/signin",
   SIGNUP = "/signup",
   ACCOUNT = "/account",
