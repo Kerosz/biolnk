aecbf9fc713c87e8dfb8d2a7ae083711eb7dc26f
Kerosz <andrei_chirila@outlook.com>
2022-01-01 15:14:04 +0200

    chore(web): update index to have biolnk branding

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,17 @@
-import { Text } from "@biolnk/ui";
+import Image from "next/image";
+import Logo from "~assets/images/biolnk.png";
+import { Flex } from "@biolnk/ui";
 
 export default function HomePage() {
   return (
-    <>
-      <Text>Landing</Text>
-    </>
+    <Flex as="main" align="center" justify="center" className="h-screen">
+      <Image
+        src={Logo}
+        height={230}
+        width={140}
+        alt="Biolnk.me branding"
+        title="Biolnk.me branding"
+      />
+    </Flex>
   );
 }
