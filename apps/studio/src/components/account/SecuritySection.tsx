98e58acc1546a93e537f7e7a36e15ca8fca1ea84
Kerosz <andrei_chirila@outlook.com>
2022-01-03 21:28:42 +0200

    feat(studio): add delete account functionality

--- apps/studio/src/components/account/SecuritySection.tsx
+++ apps/studio/src/components/account/SecuritySection.tsx
@@ -1,14 +1,38 @@
 import ConfirmDialog from "~components/ConfirmDialog";
 import useDisclosure from "~/utils/hooks/useDisclosure";
-import { Button, Heading, Text } from "@biolnk/ui";
+import useDeleteUser from "~/utils/hooks/mutations/useDeleteUser";
+import { useRouter } from "next/router";
+import { useSupabase } from "~/lib/supabase";
+import { storage } from "@biolnk/utils";
+import { Button, Heading, makeToast, Text } from "@biolnk/ui";
+import { Routes } from "~/data/enums";
 import { PageWithMetadata } from "~/types";
 import { FC } from "react";
 
 const SecuritySection: FC<PageWithMetadata["user"]> = ({ username }) => {
+  const { mutateAsync, isLoading } = useDeleteUser();
+  const { session } = useSupabase();
+  const { replace } = useRouter();
+
   const deleteDialog = useDisclosure();
 
-  const handleDeleteAccount = () => {
-    return null;
+  const handleDeleteAccount = async () => {
+    try {
+      if (session?.access_token) {
+        await mutateAsync(session?.access_token);
+
+        storage.remove("supabase.auth.token");
+
+        replace(Routes.SIGNIN);
+      }
+    } catch (error) {
+      makeToast({
+        duration: 2500,
+        kind: "error",
+        title: "Error",
+        message: error.message,
+      });
+    }
   };
 
   return (
@@ -17,6 +41,7 @@ const SecuritySection: FC<PageWithMetadata["user"]> = ({ username }) => {
         open={deleteDialog.isOpen}
         onClose={deleteDialog.onClose}
         onConfirm={handleDeleteAccount}
+        loading={isLoading}
         message={
           <p>
             Are you sure you want to delete the account, including all the data
