237d901112f8ac721246135316555ff0c94648e8
Kerosz <andrei_chirila@outlook.com>
2022-01-05 20:34:15 +0200

    chore(studio): add active state of dashboard navigation

--- apps/studio/src/components/dashboard/PageNavigation.tsx
+++ apps/studio/src/components/dashboard/PageNavigation.tsx
@@ -1,6 +1,6 @@
 import Link from "../common/Link";
+import { useRouter } from "next/router";
 import {
-  Text,
   Flex,
   BaseIcon,
   Settings,
@@ -9,38 +9,75 @@ import {
   Trello,
 } from "@biolnk/ui";
 import { Routes } from "~/data/enums/routes";
+import { ctl } from "@biolnk/utils";
 
 const PageNavigation: React.FC = () => {
+  const { pathname } = useRouter();
+
+  const getActiveLink = (value: string) => {
+    const totalNumberOfLinksOnPage = 3;
+    const paths = pathname.split("/");
+
+    /**
+     * We replace the empty string of the first entry with
+     * `links` so we can get a matching value to account
+     * for when the pathname is only "/dashboard"
+     */
+    if (paths.length < totalNumberOfLinksOnPage) {
+      paths[0] = paths[0].replace("", "links");
+    }
+
+    return paths.includes(value);
+  };
+
   return (
     <Flex justify="between" className="mb-10 border-b-2 border-mauve-600">
-      <Flex className="space-x-8 ">
+      <Flex className="space-x-5 xs:space-x-8 transform-gpu translate-y-0.5">
         <Link
           url={Routes.DASHBOARD}
-          className="pb-4 pt-1 flex items-center space-x-1.5"
+          className={ctl(
+            `pb-3 pt-1 flex items-center space-x-1.5 border-b-2 ${
+              getActiveLink("links")
+                ? "text-mauve-1000 border-mauve-1000"
+                : "text-mauve-950 border-transparent"
+            }`
+          )}
         >
           <BaseIcon icon={LinkIcon} />
 
-          <Text>Links</Text>
+          <span>Links</span>
         </Link>
         <Link
           url={Routes.PAGE}
-          className="pb-4 pt-1 flex items-center space-x-1.5"
+          className={ctl(
+            `pb-3 pt-1 flex items-center space-x-1.5 border-b-2 ${
+              getActiveLink("page")
+                ? "text-mauve-1000 border-mauve-1000"
+                : "text-mauve-950 border-transparent"
+            }`
+          )}
         >
           <BaseIcon icon={Trello} />
 
-          <Text>Page</Text>
+          <span>Page</span>
         </Link>
         <Link
-          url={Routes.ANALYTICS}
-          className="pb-4 pt-1 flex items-center space-x-1.5"
+          url={Routes.STATS}
+          className={ctl(
+            `pb-3 pt-1 flex items-center space-x-1.5 border-b-2 ${
+              getActiveLink("stats")
+                ? "text-mauve-1000 border-mauve-1000"
+                : "text-mauve-950 border-transparent"
+            }`
+          )}
         >
           <BaseIcon icon={BarChart2} />
 
-          <Text>Analytics</Text>
+          <span>Stats</span>
         </Link>
       </Flex>
 
-      <Link url={Routes.ACCOUNT} className="text-mauveDark-700">
+      <Link url={Routes.ACCOUNT} className="text-mauveDark-700 pb-1">
         <BaseIcon icon={Settings} size="lg" />
       </Link>
     </Flex>
