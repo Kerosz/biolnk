1b79e62c9935bc6075dd7951a74bcb49cd0dba54
Kerosz <andrei_chirila@outlook.com>
2022-01-09 17:26:24 +0200

    chore(studio): updated 'getPageLink' refs to use the core module

--- apps/studio/src/components/common/Header/Header.tsx
+++ apps/studio/src/components/common/Header/Header.tsx
@@ -3,9 +3,8 @@ import Logo from "~/assets/images/biolnk.png";
 import Menu from "./Menu";
 import Link from "../Link";
 import useUser from "~/utils/hooks/queries/useUser";
-import getPageLink from "~/utils/misc/getPageLink";
 import { memo } from "react";
-import { useClipboard } from "@biolnk/core";
+import { useClipboard, getPageLink } from "@biolnk/core";
 import { BaseIcon, Container, Text, Copy, Button, Flex } from "@biolnk/gamut";
 import { Routes } from "~/data/enums";
 
