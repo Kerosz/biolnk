3ed8c3a816dc1c97176b88605fa19efb6ca4c66b
Kerosz <andrei_chirila@outlook.com>
2022-01-11 14:55:53 +0200

    chore(web): include sensitive component in page index

--- apps/web/src/components/page/index.ts
+++ apps/web/src/components/page/index.ts
@@ -1,4 +1,5 @@
 export { default as PageContent } from "./PageContent";
 export { default as GoogleAnalyticsIntegration } from "./GoogleAnalytics";
+export { default as SensitiveContentAgreement } from "./SensitiveContentAgreement";
 export * from "./getStaticPaths";
 export * from "./getStaticProps";
