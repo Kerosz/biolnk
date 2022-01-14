0fbbbb83205af0397e8417063be470083288f274
Kerosz <andrei_chirila@outlook.com>
2022-01-14 21:51:36 +0200

    feat(gamut): add biolnk icon

--- packages/gamut/src/icons/index.ts
+++ packages/gamut/src/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Google } from "./Google";
 export { default as VerifiedBadge } from "./VerifiedBadge";
 export { default as CodeQR } from "./CodeQR";
+export { default as Biolnk } from "./Biolnk";
